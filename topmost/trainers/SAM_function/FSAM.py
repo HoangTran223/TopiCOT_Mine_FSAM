51cd544f88c7fa206a87b07b981e0782b97ea5cf
HoangTran223 <tranvuonghoang204@gmail.com>
2024-09-25 15:39:47 +0700

    Update TRAM

--- topmost/trainers/SAM_function/FSAM.py
+++ topmost/trainers/SAM_function/FSAM.py
@@ -6,7 +6,6 @@
         # Thêm
         self.device = device
 
-<<<<<<< HEAD
         self.sigma = sigma
         self.lmbda = lmbda
 
@@ -15,87 +14,6 @@
         self.defaults.update(self.base_optimizer.defaults)
         print ('FriendlySAM sigma:', self.sigma, 'lambda:', self.lmbda)
     
-=======
-
-class FSAM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, device, rho=0.05, adaptive=False, lr=0.002, sigma=1, lmbda=0.9): #foreach=True
-        defaults = dict(rho=rho, adaptive=adaptive, lr=lr)
-        super(FSAM, self).__init__(params, defaults)
-
-        # Thêm
-        self.device = device
-
-        self.sigma = sigma
-        self.lmbda = lmbda
-
-        self.base_optimizer = base_optimizer(self.param_groups)     # , foreach=foreach
-        self.param_groups = self.base_optimizer.param_groups
-        self.defaults.update(self.base_optimizer.defaults)
-        print ('FriendlySAM sigma:', self.sigma, 'lambda:', self.lmbda)
-
-
-    # Đoạn code ban đầu 
-    # def _grad_norm(self):
-    #     norm = torch.norm(
-    #                 torch.stack([
-    #                     ((torch.abs(p) if group["adaptive"] else 1.0) * p.grad).norm(p=2).to(self.device)
-    #                     for group in self.param_groups for p in group["params"] if p.grad is not None ]),
-    #                 p=2)
-    #     return norm
-
-
-    # @torch.no_grad()
-    # def first_step(self, zero_grad=False):
-
-    #     for group in self.param_groups:
-    #         for p in group["params"]:      
-    #             if p.grad is None: continue
-    #             # Thử
-    #             grad = p.grad
-
-    #             # grad = p.grad.clone()
-    #             if not "momentum" in self.state[p]:
-    #                 self.state[p]["momentum"] = grad
-    #             else:
-    #                 # Compute d_t
-    #                 p.grad -= self.state[p]["momentum"] * self.sigma            
-
-    #                 # Compute m_t
-    #                 self.state[p]["momentum"] = self.state[p]["momentum"] * self.lmbda + grad * (1 - self.lmbda)
-            
-    #     grad_norm = self._grad_norm()
-    #     for group in self.param_groups:
-    #         scale = group["rho"] / (grad_norm + 1e-12)
-
-    #         for p in group["params"]:
-    #             if p.grad is None: continue
-    #             self.state[p]["old_p"] = p.data.clone()
-    #             e_w = (torch.pow(p, 2) if group["adaptive"] else 1.0) * p.grad * scale.to(p)
-                
-    #             # Compute: w + e(w)
-    #             p.add_(e_w)                             
-
-    #     if zero_grad: self.zero_grad()
-
-
-    # @torch.no_grad()
-    # def second_step(self, zero_grad=False):
-    #     for group in self.param_groups:
-    #         for p in group["params"]:
-    #             if p.grad is None: continue
-
-    #             # Get back to w from w + e(w)
-    #             p.data = self.state[p]["old_p"]        
-        
-    #     # Update
-    #     self.base_optimizer.step()                      
-
-    #     if zero_grad: self.zero_grad()
-
-
-    # Kịch bản 2
-
->>>>>>> d108fec8dcd4576a1f9c168e25d53cdf9545696d
     def _grad_norm(self):
         norm = torch.norm(
                     torch.stack([
@@ -124,11 +42,7 @@ class FSAM(torch.optim.Optimizer):
         grad_norm = self._grad_norm()
 
         for group in self.param_groups:
-<<<<<<< HEAD
             scale = group["rho"] / (grad_norm + 1e-12)  
-=======
-            scale = group["rho"] / (grad_norm + 1e-12)  # Tránh chia 0
->>>>>>> d108fec8dcd4576a1f9c168e25d53cdf9545696d
             for p in group["params"]:
                 if p.grad is None: 
                     continue
@@ -150,11 +64,7 @@ class FSAM(torch.optim.Optimizer):
                 if p.grad is None: 
                     continue
 
-<<<<<<< HEAD
                 p.data.copy_(self.state[p]["old_p"])  # Khôi phục trạng thái cũ
-=======
-                p.data.copy_(self.state[p]["old_p"])  # In-place copy khôi phục trạng thái cũ
->>>>>>> d108fec8dcd4576a1f9c168e25d53cdf9545696d
 
         self.base_optimizer.step()
 
